72f6339256dbb4cd3b7500d8b800619163812632
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-13 23:59:44 -0300

    feat(Home:Hero): Create basic structure HTML for Hero component of Home page

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaRegLightbulb as IconLightBulb, FaArrowRightLong as IconArrowRight } from "react-icons/fa6";
+import { FaHtml5 as IconHTML} from "react-icons/fa";
+
+export  const Hero:React.FC = () => {
+  return (
+    <section className="hero">
+        <div className="tag">
+            <IconLightBulb />
+            <p>Tecnologia con vision</p>
+        </div>
+        <div className='hero__text'>
+            <h1>Elevamos Tu Presencia Digital Al Siguiente Nivel</h1>
+            <h4>Desarrollo Web Futurista para Empresas Visionarias</h4>
+        </div>
+        <div className="buttons">
+            <button className="button-services">
+                <p>Explorar servicios</p>
+                <IconArrowRight/>
+            </button>
+            <button className="button-contact">
+                <p>Contacto</p>
+            </button>
+        </div>
+        <div className="carousel">
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+        </div>
+    </section>
+  )
+}
