3779c700981fedf153adea66523a7c0aa22cbd91
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-15 18:41:51 -0300

    feat(ServicesHome:Home): Create basic structure for ServicesHome component of Home page

---
+++ src/components/ServicesHome/CardTag.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BiCheck as IconCheck } from 'react-icons/bi';
+export const CardTag:React.FC = () => {
+  return (
+    <div>
+        <IconCheck/>
+        <p>Responsive design</p>
+    </div>
+  )
+}
