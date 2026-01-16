5f5504db479fc23e743791061f88000acd21f65a
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-15 22:27:23 -0300

    feat(Faqs1:Home): Create basic structure for Faqs1 component of Home page

---
+++ src/components/Faqs1/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsArrowDownShort } from 'react-icons/bs'
+export const Card:React.FC = () => {
+  return (
+    <div className="card">
+        <div className="card__visible">
+            <p>¿Cuánto tarda en estar lista una página web?</p>
+            <BsArrowDownShort/>
+        </div>
+    </div>
+  )
+}
