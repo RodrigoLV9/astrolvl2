8aa40c701b453dddaf5b2981cacded3f460c927e
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-18 16:16:44 -0300

    feat(CarouselHero): Create basic structure for CarouselHero component

---
+++ src/components/CarouselHero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaHtml5 as IconHTML} from "react-icons/fa";
+export const Carousel:React.FC = () => {
+  return (
+    <div className="carouselHero">
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+            <IconHTML/>
+        </div>
+  )
+}
