a2a558cc0e6b8a64a6ef4d3716d6c5a28af66c1b
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-17 22:02:37 -0300

    style(HeroButton:Services): Add HeroButton component styling for Services page

--- src/components/ServicesPage/Hero/Button.tsx
+++ src/components/ServicesPage/Hero/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaArrowRightLong as IconArrowRight } from "react-icons/fa6";
-
+import '../../../styles/Services/button.css'
 export const Button:React.FC = () => {
   return (
     <button className="heroLeft__button">
