c223f3b43193e5ca2f47f74fbc2b65351a8c7d8b
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-17 21:52:56 -0300

    style(HeroTag:Services): Add HeroTag component styling for Services page

--- src/components/ServicesPage/Hero/HeroTag.tsx
+++ src/components/ServicesPage/Hero/HeroTag.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GiFallingStar as IconStar } from "react-icons/gi";
-
-export const HeroTag = () => {
+import '../../../styles/Services/heroTag.css'
+export const HeroTag:React.FC = () => {
   return (
     <div className="heroTag">
       <IconStar className="heroTag__icon" />
