a54cded4ed9de3a83ae7c716872760614778a43b
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-18 17:29:33 -0300

    feat(CardsIntro): Create basic structure HTML for CardsIntro component

---
+++ src/components/ServicesPage/CardsIntro/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsRocketTakeoffFill as IconRocket } from "react-icons/bs";
+
+export const Card:React.FC = () => {
+  return (
+    <div>
+        <IconRocket/>
+        <span>Conversion optimizada</span>
+        <p>Copy persuasivo y CTAs estratégicos diseñados para maximizar conversiones</p>
+    </div>
+  )
+}
