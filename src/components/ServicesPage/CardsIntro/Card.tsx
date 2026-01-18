15799d5d28525350f12b9dafc100c36f6307477a
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-18 17:42:25 -0300

    style(CardsIntro): Add basic style css for CardsIntro component of Services page

--- src/components/ServicesPage/CardsIntro/Card.tsx
+++ src/components/ServicesPage/CardsIntro/Card.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BsRocketTakeoffFill as IconRocket } from "react-icons/bs";
-
+import '../../../styles/Services/cardIntro.css'
 export const Card:React.FC = () => {
   return (
-    <div>
-        <IconRocket/>
+    <div className='cardIntro'>
+        <IconRocket className='cardIntro-icon'/>
         <span>Conversion optimizada</span>
         <p>Copy persuasivo y CTAs estratégicos diseñados para maximizar conversiones</p>
     </div>
