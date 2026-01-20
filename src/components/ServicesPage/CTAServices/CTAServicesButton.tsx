f3b942f14826ab6d8d2efbe9b55fe8c34f70022c
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-20 18:07:37 -0300

    style(CTAServices:Services): Add CTAServices component stylin g for Services page

--- src/components/ServicesPage/CTAServices/CTAServicesButton.tsx
+++ src/components/ServicesPage/CTAServices/CTAServicesButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaArrowRightLong as IconArrowRight } from "react-icons/fa6";
-
+import '../../../styles/Services/CTAServicesButton.css'
 export const CTAServicesButton:React.FC = () => {
   return (
     <button className='CTAServicesButton'>
