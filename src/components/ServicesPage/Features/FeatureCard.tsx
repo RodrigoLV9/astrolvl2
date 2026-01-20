09734b7ff2a601fdceffe0cc04a3c21c70cb2557
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-20 10:25:11 -0300

    style(Features:Services): Add Features component stylin g for Services page

--- src/components/ServicesPage/Features/FeatureCard.tsx
+++ src/components/ServicesPage/Features/FeatureCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaCheckCircle as IconCheckCircle } from "react-icons/fa";
-
+import '../../../styles/Services/FeatureCard.css'
 export const FeatureCard:React.FC = () => {
   return (
-    <li className='featureItem'>
-        <IconCheckCircle className='featureItem-icon'/>
+    <li className='featureCard'>
+        <IconCheckCircle className='featureCard-icon'/>
         <span>Diseño responsive mobile-first</span>
     </li>
   )
