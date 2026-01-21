fb0270be6db0654a3dcebc9479a6832e7f4e54d3
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-20 23:57:11 -0300

    feat(More:Services): Create basic structure for More component of Services page

---
+++ src/components/ServicesPage/More/MoreCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaArrowRightLong as IconArrowRight } from "react-icons/fa6";
+
+export const MoreCard:React.FC = () => {
+  return (
+    <div className='moreCard'>
+        <h3>Sitio Web Completo</h3>
+        <p>Ideal para empresas que necesitan múltiples páginas, blog, portafolio y presencia digital robusta.</p>
+        <button className='moreCard__button'>
+            <span>Ver Sitio Web Completo</span>
+            <IconArrowRight className='moreCard__button-icon'/>
+        </button>
+    </div>
+  )
+}
