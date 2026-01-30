497fd9b5d16bc6c5c6b1112a946d3be1963b29fd
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-30 17:05:13 -0300

    feat(IntroProcess:Process): Create basic structure HTML for IntroProcess component of Process page

---
+++ src/components/ProcessPage/Intro/CardIntroProcess.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IoPlanet as IconPlanet} from "react-icons/io5";
+
+export const CardIntroProcess = () => {
+  return (
+    <div className="cardIntroProcess">
+      <IconPlanet/>
+      <h3>100%</h3>
+      <p>Cada proyecto comienza con un análisis exhaustivo de requisitos y objetivos</p>
+    </div>
+  )
+}
