b682095a4d121a177ad71ddcfb6edcd1edc70240
RodrigoLV <rodrigolv.975@gmail.com>
2026-01-31 22:31:03 -0300

    style(IntroProcess:Process): Add IntroProcess component styling for Process page

--- src/components/ProcessPage/Intro/CardIntroProcess.tsx
+++ src/components/ProcessPage/Intro/CardIntroProcess.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { IoPlanet as IconPlanet} from "react-icons/io5";
-
+import '../../../styles/Process/cardIntro.css'
 export const CardIntroProcess = () => {
   return (
     <div className="cardIntroProcess">
-      <IconPlanet/>
+      <IconPlanet className='cardIntroProcess-icon'/>
       <h3>100%</h3>
       <p>Cada proyecto comienza con un análisis exhaustivo de requisitos y objetivos</p>
     </div>
