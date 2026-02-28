4dcfd5d7e74045c511745dc8ecd7f6764a05cae1
RodrigoLV <rodrigolv.975@gmail.com>
2026-02-27 23:59:13 -0300

    style(SelectServices): Add animation with hover

--- src/components/Header/SelectServices.tsx
+++ src/components/Header/SelectServices.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import '../../styles/selectServices.css'
+import { IoIosArrowDown as IconArrowDown } from "react-icons/io";
 export const SelectServices:React.FC = () => {
   return (
     <li className='selectServices'>
-        <span>Services</span>
+        <button className='selectServices__button'>
+            <span>Services</span>
+            <IconArrowDown className='selectServices__button-icon'/>
+        </button>
         <div className="servicesList">
             <a href="/services">Landing Page</a>
             <a href="/services">Web site</a>
