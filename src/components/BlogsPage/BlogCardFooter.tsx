1a9ce143a476688504f4e90bcdad1036c885bb11
RodrigoLV <rodrigolv.975@gmail.com>
2026-03-08 12:08:30 -0300

    feat(blog): redesign LatestBlogs section and BlogCard components

--- src/components/BlogsPage/BlogCardFooter.tsx
+++ src/components/BlogsPage/BlogCardFooter.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { IoBookOutline as IconBook } from "react-icons/io5";
 import { MdOutlineDateRange as IconDate } from "react-icons/md";
-import '../../styles/Blogs/blogPopularFooter.css'
-export const BlogCardFooter:React.FC = () => {
+import { IoArrowForward as IconArrow } from "react-icons/io5";
+import '../../styles/Blogs/blogCardFooter.css'
+
+export const BlogCardFooter: React.FC = () => {
   return (
     <div className="blogCardFooter">
-        <div className="blogCardFooter__readTime">
-            <IconBook className="blogCardFooter__readTime-icon"/>
-            <span>Lectura de 8 min</span>
+      <div className="blogCardFooter__meta">
+        <div className="blogCardFooter__meta-item">
+          <IconBook />
+          <span>8 min</span>
         </div>
-        <div className="blogCardFooter__date">
-            <IconDate className="blogCardFooter__date-icon"/>
-            <span>Hace 3 años</span>
+        <div className="blogCardFooter__meta-item">
+          <IconDate />
+          <span>Hace 3 años</span>
         </div>
+      </div>
+      <span className="blogCardFooter__cta">
+        Leer <IconArrow />
+      </span>
     </div>
   )
 }
