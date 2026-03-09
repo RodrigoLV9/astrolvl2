e85d907ede9dd19787c52d0b80f4e9d3caa5b239
RodrigoLV <rodrigolv.975@gmail.com>
2026-03-09 17:51:07 -0300

    feat(intro): Redesign Intro section with stats, gradient heading, animations and responsive layout

--- src/components/HomePage/Hero/IntroTag.tsx
+++ src/components/HomePage/Hero/IntroTag.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { PiShootingStarFill as IconStar } from "react-icons/pi";
 import '../../../styles/Home/introTag.css'
-export const IntroTag:React.FC = () => {
+
+interface IntroTagProps {
+  text?: string;
+}
+
+export const IntroTag: React.FC<IntroTagProps> = ({ text = "Innovación constante" }) => {
   return (
     <div className='introTag'>
         <IconStar className='introTag__icon'/>
-        <p>Innovacion constante</p>
+        <p>{text}</p>
     </div>
   )
 }
