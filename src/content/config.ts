bf17b63881f74075f7f555100a8a5e58629785a2
RodrigoLV <rodrigolv.975@gmail.com>
2026-04-06 23:59:53 -0300

    fix(blog): render coverImage/coverAlt in blog detail and listing views

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.date(),
-		image: z.string().optional(),
+		coverImage: image().optional(),
+		coverAlt: z.string().optional(),
+		image: image().optional(),
 		tags: z.array(z.string()),
 	}),
 });
